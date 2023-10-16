5d4df5f1593534131e45f071f0a74ce6b8a8f0e6
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-10-16 19:26:09 +0200

    deleted material UI, created the frontend for the create room page with basic layout

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CreateRoomPage = () => {
-  return <div>CreateRoomPage</div>;
+  const defaultVotes = 2;
+  const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
+  const [guestCanPause, setGuestCanPause] = useState(true);
+
+  const handleVotesChange = (e) => {
+    setVotesToSkip(e.target.value);
+  };
+
+  const handleGuestCanPauseChange = (e) => {
+    setGuestCanPause(e.target.value === "true");
+  };
+
+  const handleRoomButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: votesToSkip,
+        guest_can_pause: guestCanPause,
+      }),
+    };
+
+    fetch("/api/create-room", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  return (
+    <div className="section">
+      <div className="container">
+        <h4>Create A Room</h4>
+        <div className="playback-control">
+          <p className="control">Guest Control of Playback State</p>
+          <div>
+            <input
+              type="radio"
+              value="true"
+              name="playback"
+              defaultChecked
+              onChange={handleGuestCanPauseChange}
+            />
+            <label className="play">Play/Pause</label>
+
+            <input
+              type="radio"
+              value="false"
+              name="playback"
+              onChange={handleGuestCanPauseChange}
+            />
+            <label>No Control</label>
+          </div>
+        </div>
+        <div className="votes-control">
+          <input
+            type="number"
+            required
+            defaultValue={defaultVotes}
+            min="1"
+            onChange={handleVotesChange}
+          />
+          <label>Votes Required To Skip Song</label>
+        </div>
+        <button className="create-button" onClick={handleRoomButtonPressed}>
+          Create A Room
+        </button>
+        <Link to="/" className="back-button">
+          Back
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CreateRoomPage;
